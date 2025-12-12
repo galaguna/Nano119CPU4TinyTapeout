51f1659818dce277ada0c08de3cd6141a713f3f1
Gerardo Abel Laguna-Sanchez <65410505+galaguna@users.noreply.github.com>
2025-12-12 13:16:31 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_galaguna_NanoSys_fit (
+  tt_um_galaguna_NanoSys_fit user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
